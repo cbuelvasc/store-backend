07b0c0a6bedc3f17c73aae5e96aa4ca5805e0227
Carmelo Buelvas <cbuelvasc@gmail.com>
2021-05-07 14:44:45 -0500

    Add src/

---
+++ src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { ProductsController } from './controllers/products.controller';
+import { CategoriesController } from './controllers/categories.controller';
+import { BrandsController } from './controllers/brands.controller';
+import { CustomerController } from './controllers/customers.controller';
+import { UsersController } from './controllers/users.controller';
+import { ProductsService } from './services/products.service';
+import { CategoriesService } from './services/categories.service';
+import { BrandsService } from './services/brands.service';
+import { CustomersService } from './services/customers.service';
+import { UsersService } from './services/users.service';
+
+@Module({
+  imports: [],
+  controllers: [
+    ProductsController,
+    CategoriesController,
+    BrandsController,
+    CustomerController,
+    UsersController,
+  ],
+  providers: [
+    ProductsService,
+    CategoriesService,
+    BrandsService,
+    CustomersService,
+    UsersService,
+  ],
+})
+export class AppModule {}
